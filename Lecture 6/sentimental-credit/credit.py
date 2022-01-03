e6d65c36ed80755b905de5b2a3bb2d77e706df06
itaicas21 <itaicas21@gmail.com>
2022-01-03 13:38:45 +0200

    Credit in py w comments

---
+++ Lecture 6/sentimental-credit/credit.py
@@ -0,0 +1,37 @@
+import re
+import math
+
+def main():
+
+    numeric = re.compile('^(\d{13}|\d{15,16})$')
+    card=input("Enter Credit Card Number\n")
+    match=numeric.match(card)
+    if (match==None):
+        return print("INVALID")
+    sum=0
+    start=card[:2]
+    length=match.span()[1] # len(card) possible, just emphasizing the regex indeed worked
+    every_other_digit=True if length%2==0 else False #ternary in python
+    for i in range(length):
+        if(every_other_digit):
+            sum+=(int(card[i])*2//10)+(int(card[i])*2%10) #integer division in python
+            every_other_digit= not every_other_digit
+        else:
+            sum+=int(card[i])
+            every_other_digit= not every_other_digit
+    if length==13 and sum%10==0:
+        print ("VISA")
+    elif length==15 and sum%10==0 and (start=='37' or start=='34'):
+        print("AMEX")
+    elif length==16 and sum%10==0:
+        if start[0]=='4':
+            print("VISA")
+        elif int(start)>50 and int(start)<56:
+            print("MASTERCARD")
+        else:
+            print ("INVALID")
+    else:
+        print("INVALID")
+
+if __name__ == '__main__':
+    main()
